a2808bd12ef48a7bd50c159e883415c16de4ddbc
samuel <samuelogunduyile12@gmail.com>
2024-08-18 14:51:40 +0100

    feat: add files

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li></li>
+            <li>PropertyList</li>
+            <li>About Us</li>
+            <li>Contact Us</li>
+        </ul>
+    </nav>
+  )
+}
---
+++ src/app/data.js
@@ -0,0 +1,273 @@
+const properties = [
+    {
+      title: "Modern 2-Bedroom Apartment",
+      description: "This stunning 2-bedroom apartment is the epitome of modern living, with sleek lines, stylish finishes, and cutting-edge amenities.Some of the exceptional features of this incredible home include: Spacious open-plan living area with floor-to-ceiling windows, perfect for soaking up the city views, Gourmet kitchen with high-gloss cabinets, quartz countertops, and top-of-the-line applianceS, Large master bedroom with en-suite bathroom, walk-in closet, and plenty of natural light, Private balcony with outdoor seating area, ideal for relaxing and taking in the sights, Secure parking and 24/7 concierge service, providing added peace of mind and convenience. Contact us to schedule a viewing today and make this apartment your dream home!",
+      category: "Apartment",
+      type: "Rent",
+      location: "Plot 1A, Sunset Ultimate Boulevard, Boulevard Towers Estate,Lekki Phase 1.",
+      price: "N2,000,000.00 per year." ,
+      status: "Available"
+    },
+    {
+      title: "4-bedroom detached house with a garden",
+      description: "This incredible 4-bedroom detached house is the epitome of comfort and tranquility, perfectly situated in a desirable neighborhood. The stunning garden is a true oasis, perfect for outdoor entertaining, relaxation, and making unforgettable memories with family and friends. The interior of this wonderful home is just as impressive as the exterior, with a range of exceptional features that include: Large kitchen/dining area with high-gloss cabinets, granite countertops, and French doors leading to the garden, Four generous bedrooms, including a master suite with en-suite bathroom and walk-in closet, Expansive garden with patio area, perfect for alfresco dining and outdoor entertaining, Ample parking and a detached garage, providing plenty of storage and convenience, Beautifully landscaped front garden, complete with a driveway and pathway leading to the front door. Contact us to schedule a viewing today and make this house your dream home!",
+      category: "House",
+      type: "Sale",
+      location: "Plot 1B, Sunset Ultimate Boulevard, Apt 101, Boulevard Towers Estate, Lekki Phase 1.",
+      price: "N150,000,000.00",
+      status: "Available"
+    },
+    {
+      title: "Serviced office with stunning views.",
+      description: "This serviced office is the perfect blend of style, functionality, and flexibility, offering a unique opportunity to own a piece of commercial paradise. Take your business to new heights in this exceptional serviced office, perfectly situated in a prime location. With its stunning views and beautiful garden, this office space is the ultimate destination for entrepreneurs, startups, and established businesses alike. Some of the exceptional features of this incredible office include: Breathtaking views of the surrounding landscape from the floor-to-ceiling windows, Beautifully landscaped garden, perfect for outdoor meetings and relaxation, State-of-the-art facilities, including high-speed internet, meeting rooms, and breakout spaces, Fully equipped kitchen and lounge area, ideal for networking and collaboration. Contact us to schedule a viewing today and take your business to the next level!",
+      category: "Office",
+      type: "Rent",
+      location: "Plot 2A, Sunset Ultimate Boulevard, Boulevard Towers Estate, Lekki Phase 1.",
+      price: "N5,000,000.00 per year.",
+      status: "Available"
+    },
+    {
+        title: "3-bedroom flat",
+        description: "Welcome to this stunning 3-bedroom flat, perfectly designed for a comfortable and relaxing living experience. Located and in a serene and secure neighbourhood, this beautiful apartment offers a tranquil retreat from the hustle and bustle of city life. Features of the property: 1. 3 Spacious bedrooms with ample natural light and ventilitation. 2. 2 modern bathrooms with sleek fixtures and fittings. 3. Well equipped kitchen with ample storage and countertops. 4.Spacious living room with large windows and sliding doors leading to a cozy balcony. 5.Secure parking and 24/7 security.",
+        category: "House",
+        type: "Sale",
+        location: "Plot 2B, Sunset Ultimate Boulevard, Boulevard Towers Estate, Lekki Phase 1.",
+        price: "N45,000,000.00",
+        status: "Available"
+    },
+    {
+        title: "Penthouse apartment",
+        description: "Escape to a world of luxury and sophistication in this breathtaking penthouse apartment. Perched atop a sleek high-rise building, this stunning retreat offers unparalleled views of the city skyline. Inside, you will find a haven of elegance and refinement, perfect for relaxing, entertaining, or simply taking in the sights. Some of the exceptional features of this penthouse include: Floor-to-ceiling windows with breathtaking city views, Gourmet kitchen with high-end appliances and ample counter space, Expansive master suite with spa-inspired en-suite bathroom, Private rooftop access with infinity pool and sun deck, 24/7 concierge service for unparalleled convenience. Experience the ultimate in luxury living in this incredible penthouse apartment.",
+        category: "Apartment",
+        type: "Rent",
+        location: "Plot 3A, Cedar Creek Drive, Boulevard Towers Estate, Lekki Phase 1.",
+        price: "N1,500,000.00 per year.",
+        status: "Available"
+    },
+    {
+        title: "Elegant 4-bedroom detached house",
+        description: "Stunning 4-Bedroom Detached House for Sale. Experience luxury living in this exquisite 4-bedroom detached house, perfectly designed for comfort and style. Here are 5 amazing features of this beautiful home: 4 spacious bedrooms with en-suite bathrooms and walk-in closets, Expansive living room with high ceilings and large windows, Modern kitchen with granite countertops and top-of-the-line appliances, Private backyard with outdoor pool and BBQ area, Secure gated community with 24/7 security. Don't miss out on this incredible opportunity to own your dream home! Contact us today to schedule a viewing.",
+        category: "House",
+        type: "Sale",
+        location: "Plot 3B, Cedar Creek Drive, Boulevard Towers Estate, Lekki Phase 1.",
+        price: "N90,000,000.00",
+        status: "Available"
+    },
+    {
+        title: "Luxury 2-bedroom apartment",
+        description: "Indulge in the epitome of luxury living in this stunning 2-bedroom apartment, perfectly designed for sophistication and comfort. Here are 5 exceptional features of this beautiful home: 2 spacious bedrooms with floor-to-ceiling windows and marble en-suite bathrooms, Gourmet kitchen with high-end appliances and sleek quartz countertops, Expansive living room with soaring ceilings and private balcony with city views, 24/7 concierge service and secure underground parking, State-of-the-art fitness center and rooftop pool with sun deck. Experience the ultimate in luxury living. Contact us today to schedule a viewing!",
+        category: "Apartment",
+        type: "Rent",
+        location: "Plot 4A, Cedar Creek Drive, Boulevard Towers Estate, Lekki Phase 1.",
+        price: "N1,000,000.00 per year.",
+        status: "Available"
+    },
+    {
+        title: "Exquisite 4-bedroom duplex house",
+        description: "Experience opulence and sophistication in this breathtaking 4-bedroom duplex, perfectly designed for luxury living. Here are 5 exceptional features of this stunning home: 4 spacious bedrooms with lavish en-suite bathrooms and walk-in closets, Gourmet kitchen with high-end appliances, marble countertops, and wine cellar, Expansive living room with soaring ceilings, floor-to-ceiling windows, and private balcony, Private cinema room and game room for ultimate entertainment, Secure gated community with 24/7 security, swimming pool, and gym. Live the high life in this incredible duplex. Contact us today to schedule a viewing!",
+        category: "House",
+        type: "Rent",
+        location: "Plot 4B, Cedar Creek Drive, Boulevard Towers Estate, Lekki Phase 1.",
+        price: "N3,000,000.00 per year.",
+        status: "Available"
+    },
+    {
+        title: "Furnished 2-bedroom apartment",
+        description: "Stunning Furnished 2-Bedroom Duplex for Rent. Experience comfort and style in this beautifully furnished 2-bedroom duplex, perfectly designed for modern living. Here are 5 fantastic features of this lovely home: 2 spacious bedrooms with plush bedding, wardrobes, and en-suite bathrooms, Fully equipped kitchen with modern appliances, granite countertops, and dining area, Cozy living room with comfortable seating, flat-screen TV, and private balcony, Secure gated community with 24/7 security, swimming pool, and gym, Prime location with easy access to shopping centers, restaurants, and public transportation. Move in and feel right at home! Contact us today to schedule a viewing.!",
+        category: "Apartment",
+        type: "Rent",
+        location: "Plot 5A, Cedar Creek Drive, Boulevard Towers Estate, Lekki Phase 1.",
+        price: "N800,000.00 per year.",
+        status: "Available"
+    },
+    {
+        title: "Studio apartment",
+        description: "Cozy Studio Apartment for Rent. Experience simplicity and comfort in this charming studio apartment, perfectly designed for modern living. Here are 5 great features of this lovely home: Spacious open-plan living area with comfortable seating and flat-screen TV, Fully equipped kitchen with modern appliances and granite countertops, Large window with natural light and city views, Secure building with 24/7 security and elevator access, Prime location with easy access to shopping centers, restaurants, and public transportation. The perfect pied-à-terre! Contact us today to schedule a viewing.!",
+        category: "Apartment",
+        type: "Rent",
+        location: "Plot 5B, Cedar Creek Drive, Boulevard Towers Estate, Lekki Phase 1.",
+        price: "N650,000.00 per year.",
+        status: "Available"
+    },
+    {
+        title: "1-bedroom flat",
+        description: "Enjoy modern living in this charming 1-bedroom flat. Featuring a spacious living room, a well-equipped kitchen, a serene bedroom, and a stylish bathroom. Enjoy the convenience of ample parking and 24/7 security. Your dream home awaits!Here are Few great features of this lovely home: Spacious bedroom with large closet and en-suite bathroom, Fully equipped kitchen with modern appliances and granite countertops, Bright and airy living room with large window and comfortable seating, Secure building with 24/7 security and elevator access, Prime location with easy access to shopping centers, restaurants, and public transportation. The perfect haven for singles or couples! Contact us today to schedule a viewing.", 
+        category: "Apartment",
+        type: "Rent",
+        location: "Plot 6A, Willow Creek Place, Boulevard Towers Estate, Lekki Phase 1.",
+        price: "N750,000.00 per year.",
+        status: "Available"
+    },
+    {
+        title: "Stunning 3-bedroom bungalow",
+        description: "Discover comfort and space in this charming 3-bedroom bungalow. Featuring a modern kitchen ideal for culinary enthusiasts, a spacious living room perfect for relaxation, and three well-sized bedrooms for privacy. Enjoy outdoor living on the expansive backyard and benefit from ample parking space. This is more than a house; it's a home.! This lovely property boasts: Spacious living room with large windows, Modern kitchen with granite countertops, En-suite master bedroom with walk-in closet, Private garden with outdoor seating area, Secure parking for multiple vehicles. Don't miss out on this amazing opportunity! Contact us to schedule a viewing today!",
+        category: "House",
+        type: "Sale",
+        location: "Plot 6B, Willow Creek Place, Boulevard Towers Estate, Lekki Phase 1.",
+        price: "N40,000,000.00",
+        status: "Available"
+    },
+    {
+        title: "Beachfront property",
+        description: "This stunning beachfront property offers the ultimate coastal lifestyle, with breathtaking ocean views and direct access to the beach. Some of the incredible features of this property include: Direct access to the beach with private walkway Expansive oceanfront balcony with sunset views Gourmet kitchen with high-end appliances and sea-glass backsplash Spacious master suite with en-suite bathroom and private patio Private pool and outdoor entertaining area with BBQ station Contact us to schedule a viewing today and make your beachfront dreams a reality!",
+        category: "House",
+        type: "Sale",
+        location: "Plot 7A, Willow Creek Place, Boulevard Towers Estate, Lekki Phase 1.",
+        price: "N200,000,000.00",
+        status: "Available"
+    },
+    {
+        title: "Exlcusive 4-bedroom smart home",
+        description: "",
+        category: "House",
+        type: "Sale",
+        location: "Plot 7B, Willow Creek Place, Boulevard Towers Estate, Lekki Phase 1.",
+        price: "N150,000,000.00",
+        status: "Sold"
+    },
+    {
+        title: "A 2-bedroom condominium.",
+        description: "",
+        category: "Apartment",
+        type: "Rent",
+        location: "Plot 8A, Willow Creek Place, Boulevard Towers Estate, Lekki Phase 1.",
+        price: "N2,000,000.00 per year",
+        status: "Available"
+    },
+    {
+        title: "3-bedroom beachside bungalow",
+        description: "",
+        category: "",
+        type: "Rent",
+        location: "Plot 8B, Willow Creek Place, Boulevard Towers Estate, Lekki Phase 1.",
+        price: "N1,500,000.00 per year",
+        status: "Available",
+    },
+    {
+        title: "",
+        description: "",
+        category: "",
+        type: "Rent",
+        location: "Plot 9A, Willow Creek Place, Boulevard Towers Estate, Lekki Phase 1.",
+        price: "N2,000,000.00 per year",
+        status: "Unavailable"
+    },
+    {
+        title: "Luxurious 5-bedroom villa",
+        description: "",
+        category: "",
+        type: "Sale",
+        location: "Plot 9B, Willow Creek Place, Boulevard Towers Estate, Lekki Phase 1.",
+        price: "N125,000,000.00",
+        status: "Sold"
+    },
+    {
+        title: "",
+        description: "",
+        category: "",
+        type: "Sale",
+        location: "Plot 10A, Willow Creek Place, Boulevard Towers Estate, Lekki Phase 1.",
+        price: "N100,000,000.00",
+        status: "Available"
+    },
+    {
+        title: "Sumptuous 3-bedroom penthouse",
+        description: "",
+        category: "",
+        type: "Rent",
+        location: "Plot 10B, Willow Creek Place, Boulevard Towers Estate, Lekki Phase 1.",
+        price: "N3,500,000.00 per year",
+        status: "Unavailable"
+    },
+    {
+        title: "Spectacular 5-bedroom villa",
+        description: "",
+        category: "",
+        type: "Sale",
+        location: "Plot 11A, Foxglove Ridge, Boulevard Towers Estate, Lekki Phase 1.",
+        price: "N325,000,000.00",
+        status: "Available"
+    },
+    {
+        title: "",
+        description: "",
+        category: "",
+        type: "Rent",
+        location: "Plot 11A, Foxglove Ridge, Boulevard Towers Estate, Lekki Phase 1.",
+        price: "N1,500,000.00 per year",
+        status: "Available"
+    },
+    {
+        title: "Dazzling 4-bedroom duplex",
+        description: "",
+        category: "House",
+        type: "Sale",
+        location: "Plot 12A, Foxglove Ridge, Boulevard Towers Estate, Lekki Phase 1.",
+        price: "N75,000,000.00",
+        status: "Sold"
+    },
+    {
+        title: "2-bedroom service apartment",
+        description: "",
+        category: "",
+        type: "Rent",
+        location: "Plot 12B, Foxglove Ridge, Boulevard Towers Estate, Lekki Phase 1.",
+        price: "N2,500,000.00 per year",
+        status: "Available"
+    },
+    {
+        title: "4-bedroom smart home ",
+        description: "",
+        category: "House",
+        type: "Sale",
+        location: "Plot 13A, Foxglove Ridge, Boulevard Towers Estate, Lekki Phase 1.",
+        price: "N85,000,000.00",
+        status: "Available"
+    },
+    {
+        title: "Majestic 5-bedroom villa",
+        description: "",
+        category: "",
+        type: "Rent",
+        location: "Plot 13B, Foxglove Ridge, Boulevard Towers Estate, Lekki Phase 1.",
+        price: "N4,500,000.00 per year",
+        status: "Unavailable"
+    },
+    {
+        title: "luxurious 4-bedroom duplex ",
+        description: "",
+        category: "House",
+        type: "Sale",
+        location: "Plot 14A, Foxglove Ridge, Boulevard Towers Estate, Lekki Phase 1.",
+        price: "N90,000,000.00",
+        status: "Sold"
+    },
+    {
+        title: "5-bedroom villa ",
+        description: "",
+        category: "",
+        type: "Rent",
+        location: "Plot 14B, Foxglove Ridge, Boulevard Towers Estate, Lekki Phase 1.",
+        price: "N2,000,000.00 per year",
+        status: "Available"
+    },
+    {
+        title: "Luxurious 5-bedroom duplex",
+        description: "",
+        category: "House",
+        type: "Sale",
+        location: "Plot 15A, Foxglove Ridge, Boulevard Towers Estate, Lekki Phase 1.",
+        price: "N85,000,000.00",
+        status: "Available"
+    },
+    {
+        title: "Luxurious 3-bedroom smart-home",
+        description: "",
+        category: "House",
+        type: "Sale",
+        location: "Plot 15B, Foxglove Ridge, Boulevard Towers Estate, Lekki Phase 1.",
+        price:  "N75,000,000.00",
+        status: "Available"
+    },
+  ];
+  
